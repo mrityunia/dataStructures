7dbe8a941779e5c0d21b0911c4b58c989d1c2f17
Mrityunjoy Chowdhury <33812263+mrityunia@users.noreply.github.com>
2026-01-03 12:36:42 +0530

    Update perplexity2DArrayDay1.java

--- src/test/java/com/dsa/twoDArrays/perplexity2DArrayDay1.java
+++ src/test/java/com/dsa/twoDArrays/perplexity2DArrayDay1.java
@@ -1,15 +1,19 @@
 package com.dsa.twoDArrays;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class perplexity2DArrayDay1 {
 
 
     public static void printMatrixInSpiralOrder() {
         int[][] matrix = {
-                {  1,  2,  3,  4,  5,  6},
-                {  7,  8,  9, 10, 11, 12},
-                { 13, 14, 15, 16, 17, 18},
-                { 19, 20, 21, 22, 23, 24},
-                { 25, 26, 27, 28, 29, 30}
+                {1, 2, 3, 4, 5, 6},
+                {7, 8, 9, 10, 11, 12},
+                {13, 14, 15, 16, 17, 18},
+                {19, 20, 21, 22, 23, 24},
+                {25, 26, 27, 28, 29, 30}
         };
         int top = 0, bottom = matrix.length - 1;
         int left = 0, right = matrix[0].length - 1;
@@ -41,7 +45,55 @@ public class perplexity2DArrayDay1 {
     }
 
 
-    public static void main (String [] args) {
-        printMatrixInSpiralOrder();
+    public static void main(String[] args) {
+        SetMatrixZeroes();
+    }
+
+    public static void SetMatrixZeroes() {
+        int[][] array =
+                {{1, 2, 3},
+                        {4, 0, 6},
+                        {7, 8, 9}};
+        // find row and cols
+        int rows = array.length;
+        int cols = array[0].length;
+
+        // Step 0: copy original array to output
+        int[][] arrayOutput2 = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                arrayOutput2[i][j] = array[i][j];
+            }
+        }
+        // find the zero rows and cols
+        for (int i = 0; i < rows; i++) {
+            int foundColumn =-1;
+            boolean isZeroPresent = false;
+            for (int j = 0; j < cols; j++) {
+                if (array[i][j] == 0) {
+                    isZeroPresent = true;
+                    foundColumn = j;
+                    break;
+                }
+
+            }
+            // 1,2 => make all 1 and make 2 columns zero
+            if (isZeroPresent) {
+                // zero entire row
+                for (int j = 0; j < cols; j++) {
+                    arrayOutput2[i][j] = 0;
+                }
+
+                // zero entire column
+                for (int r = 0; r < rows; r++) {
+                    arrayOutput2[r][foundColumn] = 0;
+                }
+            }
+
+        }
+        Arrays.stream(arrayOutput2)
+                .flatMapToInt(Arrays::stream)
+                .forEach(a -> System.out.printf("%d ", a));
+
     }
 }
